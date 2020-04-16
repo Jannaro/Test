a182e3d292911fbbc6e4ab451f841c6fe406c760
Jannaro <jannaro@orange.pl>
2020-04-16 21:09:36 +0200

    Toolbar added

--- Test.js
+++ Test.js
@@ -1,2 +1,9 @@
 "use strict";
 
+function showContent(templateName) {
+  var template = document.getElementById(templateName);
+  var clone = document.importNode(template.content, true);
+  document.body.appendChild(clone);
+}
+
+showContent("template2");
